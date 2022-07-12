0b95d7cd5379684d690ac56991b74cac3404487e
x_pramaya_x <78118894+BadPramaya@users.noreply.github.com>
2022-07-12 21:21:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)════════════════\\
 //
-//          RED DRAGON v6.0
+//          x_ᴇxᴘᴇʀᴛ_x
 //
 //════════════(00)════════════════\\
 
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `x_ᴇxᴘᴇʀᴛ ʙᴏᴛ ᴀʟɪᴠᴇ_x
+My Creator is x_ᴇxᴘᴇʀᴛ ᴘʀᴀᴍᴀʏᴀ_x` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['94770611075'] //Owner number in aive msg
+global.premium = ['94770611075'] //Owner Number info
+global.ownernomer = '94770611075' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'x_ᴇxᴘᴇʀᴛ_x' //Owner Name
+global.botname = ' x_ᴇxᴘᴇʀᴛ_x' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.btnurl = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'x_ᴇxᴘᴇʀᴛ_x' //Bot Pacage Name
+global.author = 'x_ᴇxᴘᴇʀᴛ_x' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©x_ᴇxᴘᴇʀᴛ_x.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://t.me/trxexpertt' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
